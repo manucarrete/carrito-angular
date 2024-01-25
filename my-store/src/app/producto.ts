c3fdfec5ac8f3f391d9a5cd1df70b74e4343496d
manucarrete <146010216+manucarrete@users.noreply.github.com>
2024-01-25 17:49:05 +0000

    Primer commit

---
+++ my-store/src/app/producto.ts
@@ -0,0 +1,10 @@
+import { Producto } from "./producto-interface";
+
+const productos: Producto[] = [
+    {
+        nombre:"Canon 684",
+        precio: 85,
+        descripcion:"Cámara de alta calidad"
+    },
+    //...
+] 
